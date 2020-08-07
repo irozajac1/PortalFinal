cfe96fbc3b312eaf9c86306bbfa5b0297423cc03
Irma Rozajac <irma.rozajac@mibo.ba>
2020-08-07 10:52:14 +0200

    Das is das buch won Jessu Kristi..

--- PortalApi/WebApplication1/Controllers/LiteratureController.cs
+++ PortalApi/WebApplication1/Controllers/LiteratureController.cs
@@ -43,7 +43,7 @@ namespace WebApplication1.Controllers
             return Ok(literature);
         }
 
-        //[HttpGet("{id}")]
+        [HttpGet("{id}")]
         public IActionResult Download(Guid id)
         {
             return service.DownloadFile(id);
